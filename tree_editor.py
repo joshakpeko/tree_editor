49268d1cebd7325aa27b081f04fbbcb70b392e69
Josh Akpeko <joshuaakpeko31@gmail.com>
2018-06-02 10:42:45 +0000

    + added LICENCE file + renamed treeEditor.py to tree_editor.py

---
+++ tree_editor.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+""" miniscript to recursively rename subdirs and filenames in a given
+    root directory.
+    The specified regex or a self-defined one help replace(or remove)
+    annoying substrings of the filenames.
+    Ex: "[ Torrenteleven.dummy ] Do_You_Trust_This_Computer.mp4"
+    can be renamed to "Do_You_Trust_This_Computer.mp4".
+"""
+
+import os, re
+import os.path as path
+
+wd = os.getcwd()
+regex = re.compile(r'^(\[[^]]*\]\s*)')
+
+def main():
+    """ Navigate top-down the directories tree and rename directories
+        and files which name matches regex."""
+
+    for root, dirs, files in os.walk(wd):
+        for d in dirs:
+            d = path.join(root, d)
+            newname = rename(d, '')
+            if newname:
+                dirs[dirs.index(path.basename(d))] = newname
+        for f in files:
+            f = path.join(root, f)
+            newname = rename(f, '')
+            if newname:
+                files[files.index(path.basename(f))] = newname
+
+def rename(filename, repl, pattern=regex):
+    """ Subitute with repl, a substring of filename matching pattern.
+        Rename the file with the new made string.
+        Return True if the rename was successful. False otherwise."""
+
+    basename = path.basename(filename)
+
+    if pattern.match(basename):
+        newname = pattern.sub(repl, basename)
+        if newname:
+            newpath = path.join(path.dirname(filename), newname)
+            try:
+                os.rename(filename, newpath)
+            except OSError as exc:
+                print(exc)
+                return None
+            else:
+                return newname
+    return None
+
+
+if __name__ == '__main__':
+    main()
